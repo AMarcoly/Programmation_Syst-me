c32710414658b048ca445f5f42b6be7f31d355d8
marcoly Antoine <marcoly.antoine@etu.unistra.fr>
2023-01-30 22:24:21 +0100

    Avancee

---
+++ TP1/exercice_1/src/raler.c
@@ -0,0 +1,24 @@
+#include "../include/raler.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdnoreturn.h>
+#include <stdlib.h>
+
+//Prq je n'arrive pas a les enlever
+
+#define CHK(op) do { if ( (op) == -1) raler (1,#op);  } while(0)
+
+
+noreturn void raler(int syserr,  const char *msg, ...){
+    va_list ap;
+
+    va_start(ap, msg);
+    vfprintf(stderr,msg, ap);
+    fprintf(stderr, "\n");
+    va_end(ap);
+
+    if(syserr == 1)
+        perror("");
+
+    exit(EXIT_FAILURE);
+}
