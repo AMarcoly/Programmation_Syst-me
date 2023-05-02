a81ee85f25528b97bbda9a77e6e98a07f98f34a9
ANTOINE MARCOLY <marcoly.antoine@etu.unistra.fr>
2023-05-03 00:05:26 +0200

    make

---
+++ TP10/alarm.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+#define CHK(op) do { if ( (op) == -1) raler (#op);  } while(0)
+
+
+
+/**
+ * @brief Fonction raler qui va s'occuper d'afficher les erreurs sur la sortie erreur standard
+ * 
+ * @param msg 
+ */
+
+void raler(const char * msg){
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+
